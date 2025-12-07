8ceaec5a241b7386b651a316df909e0afc0d5be4
Kevin Sloan <61387934+KJSloan2@users.noreply.github.com>
2025-12-07 11:29:39 -0600

    Added three_dep table and edited variable names of other tables.

--- src/sql/sqlCreateTempGeoDb.py
+++ src/sql/sqlCreateTempGeoDb.py
@@ -1,6 +1,13 @@
 import os
 import sqlite3
 
+args = {
+    "make_osm_srf_table": True,
+    "make_composite_table": True,
+    "make_spectral_table": True,
+    "make_threeDep_table": True
+}
+
 conn = sqlite3.connect('tempGeo.db')
 
 def create_table(table_name, columns):
@@ -14,43 +21,90 @@ def create_table(table_name, columns):
     conn.commit()
     print(table_name)
 
-#"railway", "waterway", "aeroway", "powerline", "pipeline"
-'''osmFeatureTags = ["highway_srf", "building_srf", "construction_srf"]
-for tag in osmFeatureTags:
-    create_table(tag, {
+if args["make_osm_srf_table"]:
+    #"railway", "waterway", "aeroway", "powerline", "pipeline"
+    osmFeatureTags = ["highway_srf", "building_srf", "construction_srf"]
+    for tag in osmFeatureTags:
+        create_table(tag, {
+            'geoid': 'INTEGER PRIMARY KEY',
+            'lat': 'FLOAT',
+            'lon': 'FLOAT', 
+            'srf': 'TEXT', 
+            })
+
+if args["make_composite_table"]:
+    # Create table for 3Dep + LS8 composite data
+    create_table('terrainComposite', {
         'geoid': 'INTEGER PRIMARY KEY',
         'lat': 'FLOAT',
         'lon': 'FLOAT', 
-        'surface': 'FLOAT', 
-        })'''
+        'lstf': 'FLOAT',
+        'lstf_serc': 'FLOAT', 
+        'lstf_arc': 'FLOAT',
+        'lstf_flag': 'TEXT', 
+        'ndvi': 'FLOAT', 
+        'ndvi_serc': 'FLOAT', 
+        'ndvi_arc': 'FLOAT', 
+        'ndvi_flag': 'TEXT', 
+        'ndmi': 'FLOAT',
+        'ndmi_serc': 'FLOAT',
+        'ndmi_arc': 'FLOAT',
+        'ndmi_flag': 'TEXT',
+        'elv_rel': 'FLOAT',
+        'elv': 'FLOAT',
+        'idx_row': 'INTEGER',
+        'idx_col': 'INTEGER',
+        "mdrdasp": 'FLOAT',
+        "mdrdconc": 'FLOAT',
+        "mdrdgrv": 'FLOAT',
+        "mdrdunp": 'FLOAT',
+        "mdconst": 'FLOAT',
+        "mdbldg": 'FLOAT',
+        'lstf_ndvi_corr': 'FLOAT',
+        'lstf_ndmi_corr': 'FLOAT', 
+        'ndvi_ndmi_corr': 'FLOAT',
+        'lstf_ndvi_pval': 'FLOAT', 
+        'lstf_ndmi_pval': 'FLOAT', 
+        'ndvi_ndmi_pval': 'FLOAT'
+        })
 
-# Create table for 3Dep + LS8 composite data
-create_table('terrainComposite', {
-    'geoid': 'INTEGER PRIMARY KEY',
-    'lat': 'FLOAT',
-    'lon': 'FLOAT', 
-    'lstf': 'FLOAT',
-    'lstf_serc': 'FLOAT', 
-    'lstf_arc': 'FLOAT',
-    'lstf_flag': 'TEXT', 
-    'ndvi': 'FLOAT', 
-    'ndvi_serc': 'FLOAT', 
-    'ndvi_arc': 'FLOAT', 
-    'ndvi_flag': 'TEXT', 
-    'ndmi': 'FLOAT',
-    'ndmi_serc': 'FLOAT',
-    'ndmi_arc': 'FLOAT',
-    'ndmi_flag': 'TEXT',
-    'elv_rel': 'FLOAT',
-    'elv': 'FLOAT',
-    'idx_row': 'INTEGER',
-    'idx_col': 'INTEGER',
-    "mdrdasp": 'FLOAT',
-    "mdrdconc": 'FLOAT',
-    "mdrdgrv": 'FLOAT',
-    "mdrdunp": 'FLOAT',
-    "mdconst": 'FLOAT',
-    "mdbldg": 'FLOAT',
-    })
+if args["make_spectral_table"]:
+    # Create table for 3Dep + LS8 composite data
+    create_table('spectral_temporal', {
+        'geoid': 'INTEGER PRIMARY KEY',
+        'lat': 'FLOAT',
+        'lon': 'FLOAT', 
+        'lstf': 'FLOAT',
+        'lstf_serc': 'FLOAT', 
+        'lstf_arc': 'FLOAT',
+        'ndvi': 'FLOAT', 
+        'ndvi_serc': 'FLOAT', 
+        'ndvi_arc': 'FLOAT', 
+        'ndmi': 'FLOAT',
+        'ndmi_serc': 'FLOAT',
+        'ndmi_arc': 'FLOAT',
+        'idx_row': 'INTEGER',
+        'idx_col': 'INTEGER',
+        'lstf_ndvi_corr': 'FLOAT',
+        'lstf_ndmi_corr': 'FLOAT', 
+        'ndvi_ndmi_corr': 'FLOAT',
+        'lstf_ndvi_pval': 'FLOAT', 
+        'lstf_ndmi_pval': 'FLOAT', 
+        'ndvi_ndmi_pval': 'FLOAT'
+        })
 
+if args["make_threeDep_table"]:
+    # Create table for 3Dep + LS8 composite data
+    create_table('three_dep', {
+        'rowId': 'INTEGER PRIMARY KEY',
+        'idx_row': 'INTEGER',
+        'idx_col': 'INTEGER',
+        'lat': 'FLOAT',
+        'lon': 'FLOAT',
+        'elv_rel': 'FLOAT',
+        'elv': 'FLOAT',
+        'slope': 'FLOAT',
+        })
+
+    
 conn.close()
