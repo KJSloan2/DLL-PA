d9999efbb8e64eff6e4d402a2d5c07da932f64a9
Kevin Sloan <61387934+KJSloan2@users.noreply.github.com>
2026-01-19 10:16:21 -0600

    Updated path references

--- src/databases/runtime/dbAppend_runtime_dirLib.py
+++ src/databases/runtime/dbAppend_runtime_dirLib.py
@@ -1,5 +1,3 @@
-import os
-import sys
 import sqlite3
 
 conn = sqlite3.connect('runtime.db')
@@ -9,29 +7,42 @@ TABLE_NAME = 'dir_lib'
 CLEAR_TABLE = True
 if CLEAR_TABLE:
     cursor.execute(f"DELETE FROM {TABLE_NAME}")
+    
+USER_NAME = ""
 
 dataToAdd = [
     {
         "DIR_NAME":"USGS_WSS", 
-        "DIR_PATH":"C:\\Users\\Kjslo\\Documents\\data\\USGS\\Web_Soil_Survey", 
+        "DIR_PATH":f"C:\\Users\\{USER_NAME}\\Documents\\data\\USGS\\Web_Soil_Survey", 
         "USEAGE":"Soil data reference"
         },
 	{
         "DIR_NAME":"USCB_HYDROGRAPHY_TIGER", 
-        "DIR_PATH":"C:\\Users\\Kjslo\\Documents\\data\\USCB\\Tiger\\Hydrography", 
+        "DIR_PATH":f"C:\\Users\\{USER_NAME}\\Documents\\data\\USCB\\Tiger\\Hydrography", 
         "USEAGE":"Body of water spatial reference"
     },
     {
         "DIR_NAME":"USCB_TIGER_TABULATION_US", 
-        "DIR_PATH":"C:\\Users\\Kjslo\\Documents\\data\\USCB\\Tiger\\Tabulation_US", 
+        "DIR_PATH":f"C:\\Users\\{USER_NAME}\\Documents\\data\\USCB\\Tiger\\Tabulation_US", 
         "USEAGE":"Tabulation areas for entire US"
     },
     {
         "DIR_NAME":"USDA_NASS_CDL_2024_30m", 
-        "DIR_PATH":"C:\\Users\\Kjslo\\Documents\\data\\USDA\\2024_30m_cdls", 
+        "DIR_PATH":f"C:\\Users\\{USER_NAME}\\Documents\\data\\USDA\\2024_30m_cdls", 
         "USEAGE":"USDA Cropland Data Layer 2024 30m resolution"
+    },
+    {
+        "DIR_NAME":"FRONTEND_BASE", 
+        "DIR_PATH":f"C:\\Users\\{USER_NAME}\\Documents\\local_dev\\dynamic_lands_lab", 
+        "USEAGE":"Frontend UI app"
+    },
+    {
+        "DIR_NAME":"PROCESSED_ARCHIVE", 
+        "DIR_PATH":f"C:\\Users\\{USER_NAME}\\Documents\\data\\DLL_Preprocessed", 
+        "USEAGE":"Log processed data to archive"
     }
 ]
+
 
 for data in dataToAdd:
     print(data)
