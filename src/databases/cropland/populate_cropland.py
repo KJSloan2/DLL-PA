b579e7e0c7a6ba9fb1606f7e621b34ef65a9607c
Kevin Sloan <61387934+KJSloan2@users.noreply.github.com>
2026-02-03 05:50:41 -0600

    Updated database from sqlite3 to duckDb

--- src/databases/cropland/populate_cropland.py
+++ src/databases/cropland/populate_cropland.py
@@ -2,20 +2,22 @@ import os
 import sys
 import csv
 import json
-import sqlite3
+import duckdb
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-from utils import hex_to_rgb
+from global_functions.utils import hex_to_rgb
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 RESET_TABLE = True
+DIR_PATH = r"C:\Users\Kjslo\Documents\data\USDA\2024_30m_cdls"
+RASTER_FNAME = "USDA_NASS_CDL-landCoverClassifications.csv"
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
-csv_path = os.path.join(r"C:\Users\Kjslo\Documents\data\USDA\2024_30m_cdls", "USDA_NASS_CDL-landCoverClassifications.csv")
+csv_path = os.path.join(DIR_PATH, RASTER_FNAME)
 csv_file = open(csv_path, mode='r', newline='', encoding='utf-8-sig')
 csv_reader = csv.DictReader(csv_file)
-
-DB_NAME = 'usda_nass_cdl.db'
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
+DB_NAME = 'usda_nass_cdl.duckdb'
 TABLE_NAME = 'land_cover_classification_ref'
-conn = sqlite3.connect(DB_NAME)
+conn = duckdb.connect(DB_NAME)
 cursor = conn.cursor()
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - #
 # If RESET_TABLE is true, delete all existing records in the table
