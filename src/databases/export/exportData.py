27bd68e52e54cb79d9f2f830378ff38244a81ffc
Kevin Sloan <61387934+KJSloan2@users.noreply.github.com>
2026-03-18 05:05:30 -0500

    Updated to break exported data into specified max file sizes. Splits data into tiles for efficient uploading and reading.

--- src/databases/export/exportData.py
+++ src/databases/export/exportData.py
@@ -1,10 +1,15 @@
 import os
+import sys
 import io
+import math
 import csv
 import json
 import shutil
 import sqlite3
 import duckdb
+##################################################################################
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from global_functions.utils import  json_serialize
 ##################################################################################
 MAX_FILE_SIZE_MB = 23.5
 ##################################################################################
@@ -28,6 +33,7 @@ else:
 query = "SELECT * FROM site_info"
 cursor.execute(query)
 siteInfo = cursor.fetchone()
+print(siteInfo)
 headers = [description[0] for description in cursor.description]
 siteInfo = dict(zip(headers, siteInfo))
 locationId = siteInfo['NAME']
@@ -45,7 +51,9 @@ FIELD_FILTERS = {
         'geoid', 'lat', 'lon', 
         'lstf', 'lstf_serc', 'lstf_arc', 
         'ndvi', 'ndvi_serc', 'ndvi_arc', 
-        'ndmi', 'ndmi_serc', 'ndmi_arc', 
+        'ndmi', 'ndmi_serc', 'ndmi_arc',
+        'lstf_temporal', 'ndvi_temporal', 'ndmi_temporal',
+        'lstf_flag', 'ndvi_flag', 'ndmi_flag',
         'elv_rel', 'elv', 
         'mdrdasp', 'mdrdconc', 'mdrdgrv', 'mdrdunp', 'mdconst', 'mdbldg', 
         'lstf_ndvi_corr', 'lstf_ndmi_corr', 'ndvi_ndmi_corr', 'lstf_ndvi_pval', 'lstf_ndmi_pval', 'ndvi_ndmi_pval', 
@@ -67,7 +75,7 @@ dbExportMapping = [
     },
     {
         "db_name": "tempGeo",
-        "field_filter": "mapbox",
+        "field_filter": "vercel",
         "table": "terrain_composite", 
         "export_as": ["csv", "geojson"],
         "output_dir": os.path.join("output", "multiSpec"),
@@ -113,7 +121,18 @@ else:
     rows = conn.execute(f"SELECT * FROM {tableName}").fetchall()
     headers = [row[1] for row in conn.execute(f"PRAGMA table_info('{tableName}')").fetchall()]
 
-rowCount = len(rows)
+validRows = []
+for row in rows:
+    nullCount = 0
+    for val in row:
+        #if val is None or (isinstance(val, float) and math.isnan(val)):
+        if val is None or val == "None" or (isinstance(val, float) and math.isnan(val)):
+            nullCount += 1
+    #print(nullCount)
+    if nullCount <= 10: 
+        validRows.append(row)
+
+rowCount = len(validRows)
 
 def write_to_csv(path, headers, rowsToWrite):
     with open(path, mode='w', newline='', encoding='utf-8') as file:
@@ -122,10 +141,19 @@ def write_to_csv(path, headers, rowsToWrite):
         for row in rowsToWrite:
             writer.writerow(row)
 
+def replace_nulls(data):
+    if isinstance(data, dict):
+        return {k: (0 if v is None or (isinstance(v, float) and math.isnan(v)) else v) for k, v in data.items()}
+    elif isinstance(data, (list, tuple)):
+        return [0 if v is None or (isinstance(v, float) and math.isnan(v)) else v for v in data]
+    else:
+        return data
+
 def write_to_geojson(path, headers, rowsToWrite):
     features = []
     for row in rowsToWrite:
         row_dict = dict(zip(headers, row))
+        row_dict = replace_nulls(row_dict)
         feature = {
             "type": "Feature",
             "properties": {k: v for k, v in row_dict.items() if k not in ['lat', 'lon']},
@@ -141,8 +169,8 @@ def write_to_geojson(path, headers, rowsToWrite):
         "features": features
     }
 
-    with open(path, 'w', encoding='utf-8') as f:
-        json.dump(geojson, f, ensure_ascii=False)
+    with open(path, "w", encoding='utf-8') as f:
+        f.write(json.dumps(geojson, indent=1, default=json_serialize, ensure_ascii=False))
 
 def csv_row_size_bytes(row):
     """
@@ -151,6 +179,7 @@ def csv_row_size_bytes(row):
     """
     buffer = io.StringIO()
     writer = csv.writer(buffer, lineterminator="\n")
+    row = replace_nulls(row)
     writer.writerow(row)
     return len(buffer.getvalue().encode("utf-8"))
 
@@ -160,7 +189,7 @@ header_writer = csv.writer(header_buffer, lineterminator="\n")
 header_writer.writerow(headers)
 header_size = len(header_buffer.getvalue().encode("utf-8"))
 
-row_sizes = [csv_row_size_bytes(row) for row in rows]
+row_sizes = [csv_row_size_bytes(row) for row in validRows]
 avgRowSize = sum(row_sizes) / len(row_sizes) if row_sizes else 0
 
 maxFileSizeBytes = MAX_FILE_SIZE_MB * 1024 * 1024
@@ -177,7 +206,7 @@ if rowCount > rowsPerFile:
     for i in range(neededFiles):
         startIdx = i * rowsPerFile
         endIdx = min(startIdx + rowsPerFile, rowCount)
-        rowChunk = rows[startIdx:endIdx]
+        rowChunk = validRows[startIdx:endIdx]
 
         if "csv" in exportAs:
             output_path = os.path.join(mapping["output_dir"], output_fName+"_"+str(i)+".csv")
@@ -186,13 +215,15 @@ if rowCount > rowsPerFile:
             output_path = os.path.join(mapping["output_dir"], output_fName+".geojson")
             write_to_geojson(output_path, headers, rowChunk)
 
-else:
-    if "csv" in exportAs:
-        output_path = os.path.join(mapping["output_dir"], output_fName+".csv")
-        write_to_csv(output_path, headers, rows)
-    if "geojson" in exportAs:
-        output_path = os.path.join(mapping["output_dir"], output_fName+".geojson")
-        write_to_geojson(output_path, headers, rows)
+# Export full data in one file
+if "csv" in exportAs:
+    output_path = os.path.join(mapping["output_dir"], output_fName+"_allTiles.csv")
+    write_to_csv(output_path, headers, validRows)
+if "geojson" in exportAs:
+    output_path = os.path.join(mapping["output_dir"], output_fName+"_allTiles.geojson")
+    write_to_geojson(output_path, headers, validRows)
+
+
 
 conn.close()
 print(f"{dbName} - {tableName} exported to CSV")
