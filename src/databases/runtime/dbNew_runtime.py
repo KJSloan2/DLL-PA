cc1c0d044532c12c80c46decdf77e23971673722
Kevin Sloan <61387934+KJSloan2@users.noreply.github.com>
2026-03-18 05:03:25 -0500

    Added "tiles" table to track exported tile bounds and field filters applied.

--- src/databases/runtime/dbNew_runtime.py
+++ src/databases/runtime/dbNew_runtime.py
@@ -2,39 +2,43 @@ import os
 import sys
 import sqlite3
 ###################################################################
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from sqlite_utils import create_table
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from global_functions.sqlite_utils import create_table
 ###################################################################
 DROP = True
 ###################################################################
-args = {
-    "make_dir_lib_table": True,
-    "make_site_info_table": True
-}
-
 conn = sqlite3.connect('runtime.db')
 ###################################################################
-if args["make_dir_lib_table"]:
-    create_table(conn, 'dir_lib', {
-        'DIR_NAME': 'TEXT PRIMARY KEY',
-        'DIR_PATH': 'TEXT',
-        'USEAGE': 'TEXT',
-        }, DROP)
-    
-if args["make_site_info_table"]:
-    create_table(conn, 'site_info', {
-        'NAME': 'TEXT PRIMARY KEY',
-        'STATE_NAME': 'TEXT',
-        'STATE_FIPS': 'TEXT',
-        'COUNTY_NAME': 'TEXT',
-        'COUNTY_FIPS': 'TEXT',
-        'PLACE_NAME': 'TEXT',
-        'PLACE_GEOID': 'TEXT',
-        'HAS_HYDRO_FEATURES': 'TEXT',
-        'AOI_BB_PT_SW': 'TEXT',
-        'AOI_BB_PT_SE': 'TEXT',
-        'AOI_BB_PT_NW': 'TEXT',
-        'AOI_BB_PT_NE': 'TEXT',
-        'AOI_CENTROID': 'TEXT'
-        }, DROP)
+create_table(conn, 'dir_lib', {
+    'DIR_NAME': 'TEXT PRIMARY KEY',
+    'DIR_PATH': 'TEXT',
+    'USEAGE': 'TEXT',
+    }, DROP)
+
+create_table(conn, 'site_info', {
+    'NAME': 'TEXT PRIMARY KEY',
+    'STATE_NAME': 'TEXT',
+    'STATE_FIPS': 'TEXT',
+    'COUNTY_NAME': 'TEXT',
+    'COUNTY_FIPS': 'TEXT',
+    'PLACE_NAME': 'TEXT',
+    'PLACE_GEOID': 'TEXT',
+    'HAS_HYDRO_FEATURES': 'TEXT',
+    'AOI_BB_PT_SW': 'TEXT',
+    'AOI_BB_PT_SE': 'TEXT',
+    'AOI_BB_PT_NW': 'TEXT',
+    'AOI_BB_PT_NE': 'TEXT',
+    'AOI_CENTROID': 'TEXT'
+    }, DROP)
+
+create_table(conn, 'tiles', {
+    'NAME': 'TEXT PRIMARY KEY',
+    'TILE_ID': 'TEXT',
+    'FILTER_SCHEME': 'TEXT',
+    'AOI_BB_PT_SW': 'TEXT',
+    'AOI_BB_PT_SE': 'TEXT',
+    'AOI_BB_PT_NW': 'TEXT',
+    'AOI_BB_PT_NE': 'TEXT'
+    }, DROP)
+
 conn.close()
